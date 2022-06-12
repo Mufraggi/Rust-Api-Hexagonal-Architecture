22202b4ee8170e321bdb3fe324b04931a05c007f
hugomufraggi <Mufraggi0303>
2022-06-12 11:06:23 +0200

    Start the implementation of the contract

---
+++ src/lib.rs
@@ -0,0 +1,94 @@
+use sqlx::postgres::PgPool;
+use sqlx::{Error, Pool, Postgres};
+
+pub enum InsertError {
+    Conflict,
+    Unknown,
+}
+
+pub enum FetchAllError {
+    Unknown,
+}
+
+pub enum FetchOneError {
+    NotFound,
+    Unknown,
+}
+
+pub enum DeleteError {
+    NotFound,
+    Unknown,
+}
+
+pub struct PostgresRepository {
+    db_pool: Pool<Postgres>,
+}
+
+pub struct DbUser {
+    id:String,
+    first_name: String,
+    last_name : String,
+    birthday_date: Date,
+    city: String
+}
+
+impl PostgresRepository {
+    async fn new(mut self, url_db: String) -> Result<(),String> {
+        let tmp = PgPool::connect(&url_db).await;
+        match tmp {
+            Ok(value) => {
+                self.db_pool = value;
+                Ok(())
+            }
+            Err(err) => {
+                Err("blabla".to_string())
+            }
+        }
+    }
+}
+
+pub trait Repository: Send + Sync {
+    fn insert(
+        &self,
+        number: u32,
+        name: String,
+    ) -> anyhow::Result<DbUser, InsertError>;
+    fn fetch_all(&self) -> anyhow::Result<Vec<DbUser>, FetchAllError>;
+
+    fn fetch_one(&self, number: u32) -> anyhow::Result<DbUser, FetchOneError>;
+    fn update(&self, id: string, new_db_user: DbUser) -> anyhow::Result<DbUser, FetchAllError>;
+    fn delete(&self, number: u32) -> anyhow::Result<(), DeleteError>;
+}
+
+impl Repository for PostgresRepository {
+    fn insert(&self, number: u32, name: String) -> anyhow::Result<DbUser, InsertError> {
+        todo!()
+    }
+
+    fn fetch_all(&self) -> anyhow::Result<Vec<DbUser>, FetchAllError> {
+        todo!()
+    }
+
+    fn fetch_one(&self, number: u32) -> anyhow::Result<DbUser, FetchOneError> {
+        todo!()
+    }
+
+    fn update(&self, id: string, new_db_user: DbUser) -> anyhow::Result<DbUser, FetchAllError> {
+        todo!()
+    }
+
+    fn delete(&self, number: u32) -> anyhow::Result<(), DeleteError> {
+        todo!()
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
