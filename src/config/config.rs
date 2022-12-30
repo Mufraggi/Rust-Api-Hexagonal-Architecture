1316dfec467669135062f24a44b1f57a62ff0b49
hugomufraggi <Mufraggi0303>
2022-12-30 12:40:35 +0100

    feat/add ci
    try to fixe the task

--- src/config/config.rs
+++ src/config/config.rs
@@ -11,6 +11,7 @@ pub struct Config {
 
 pub fn get_config() -> Config {
     let url_postgres = env::var("DATABASE_URL").unwrap_or_else(|_| "postgres://postgres:somePassword@localhost:5432/postgres".to_string());
+    println!("{}", url_postgres.to_string());
     let url_domain = env::var("URL_DOMAIN").unwrap_or_else(|_| "localhost".to_string());
     Config{url_postgres, url_domain }
         /*match (env::var("DATABASE_URL"), ) {
